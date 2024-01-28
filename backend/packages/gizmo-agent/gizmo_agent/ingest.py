46510b3ecb2ba48dc2687bd6b07d62f5396844b4
Lorenz Helle <lorenzhelle@web.de>
2024-01-28 16:45:18 +0100

    Update embeddings import in ingest.py

--- backend/packages/gizmo-agent/gizmo_agent/ingest.py
+++ backend/packages/gizmo-agent/gizmo_agent/ingest.py
@@ -1,7 +1,8 @@
 import os
 
 from agent_executor.upload import IngestRunnable
-from langchain_community.embeddings import AzureOpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
+
 from langchain.schema.runnable import ConfigurableField
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.redis import Redis
@@ -13,10 +14,9 @@ index_schema = {
 vstore = Redis(
     redis_url=os.environ["REDIS_URL"],
     index_name="opengpts",
-    embedding=AzureOpenAIEmbeddings(engine=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"]),
+    embedding=OpenAIEmbeddings(),
     index_schema=index_schema,
 )
-
 
 ingest_runnable = IngestRunnable(
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200),
